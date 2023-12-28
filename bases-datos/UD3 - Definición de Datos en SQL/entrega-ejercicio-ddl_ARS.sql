c28cf37f4a660feda4fb84291381958f5e882242
Alberto Requena <albertorekena.dev@gmail.com>
2023-12-28 04:48:54 +0100

    bases-datos/UD3 - Definicion de Datos en SQL/entrega-ejercicio-ddl_ARS.sql Terminada

--- bases-datos/UD3 - Definición de Datos en SQL/entrega-ejercicio-ddl_ARS.sql
+++ bases-datos/UD3 - Definición de Datos en SQL/entrega-ejercicio-ddl_ARS.sql
@@ -6,6 +6,7 @@ DROP TABLE linea CASCADE CONSTRAINTS;
 DROP TABLE elemento_de_coste CASCADE CONSTRAINTS;
 DROP TABLE concepto_retributivo CASCADE CONSTRAINTS;
 DROP TABLE ingreso CASCADE CONSTRAINTS;
+DROP TABLE descuento CASCADE CONSTRAINTS;
 
 
 CREATE TABLE departamento(
@@ -67,6 +68,16 @@ CREATE TABLE ingreso(
     codigoConcepto_retributivo NUMBER(6)
 );
 
+CREATE TABLE descuento(
+    num_lineaLinea VARCHAR2(200),
+    ejercicioNomina DATE,
+    mesNomina NUMBER(6),
+    numeroNomina NUMBER(6),
+    num_matEmpleado NUMBER(5),
+    base NUMBER(8, 2),
+    porcentaje NUMBER(5, 2)
+);
+
 
 ALTER TABLE departamento ADD CONSTRAINT pk_departamento PRIMARY KEY(codigo);
 ALTER TABLE empleado ADD CONSTRAINT pk_empleado PRIMARY KEY(num_mat);
@@ -76,6 +87,7 @@ ALTER TABLE linea ADD CONSTRAINT pk_linea PRIMARY KEY(num_linea, ejercicioNomina
 ALTER TABLE elemento_de_coste ADD CONSTRAINT pk_ele_de_cos PRIMARY KEY(codigo);
 ALTER TABLE concepto_retributivo ADD CONSTRAINT pk_con_ret PRIMARY KEY(codigo);
 ALTER TABLE ingreso ADD CONSTRAINT pk_ingreso PRIMARY KEY(num_lineaLinea, ejercicioNomina, mesNomina, numeroNomina, num_matEmpleado);
+ALTER TABLE descuento ADD CONSTRAINT pk_descuento PRIMARY KEY(num_lineaLinea, ejercicioNomina, mesNomina, numeroNomina, num_matEmpleado);
 
 ALTER TABLE empleado_departamento ADD CONSTRAINT fk_emp_dep_cod FOREIGN KEY(codigoDepartamento) REFERENCES departamento(codigo) ON DELETE CASCADE;
 ALTER TABLE empleado_departamento ADD CONSTRAINT fk_emp_dep_num FOREIGN KEY(num_matEmpleado) REFERENCES empleado(num_mat) ON DELETE CASCADE;
@@ -84,3 +96,6 @@ ALTER TABLE linea ADD CONSTRAINT fk_lin_eje_mes_numNom_numEmp FOREIGN KEY(ejerci
 ALTER TABLE elemento_de_coste ADD CONSTRAINT fk_ele_per FOREIGN KEY(pertenece_elemento) REFERENCES elemento_de_coste(codigo) ON DELETE CASCADE;
 ALTER TABLE ingreso ADD CONSTRAINT fk_ing_num_eje_mes_numNom_numEmp FOREIGN KEY(num_lineaLinea, ejercicioNomina, mesNomina, numeroNomina, num_matEmpleado) REFERENCES linea(num_linea, ejercicioNomina, mesNomina, numeroNomina, num_matEmpleado) ON DELETE CASCADE;
 ALTER TABLE ingreso ADD CONSTRAINT fk_ing_cod FOREIGN KEY(codigoConcepto_retributivo) REFERENCES concepto_retributivo(codigo) ON DELETE CASCADE;
+ALTER TABLE descuento ADD CONSTRAINT fk_des_num_eje_mes_numNom_numEmp FOREIGN KEY(num_lineaLinea, ejercicioNomina, mesNomina, numeroNomina, num_matEmpleado) REFERENCES linea(num_linea, ejercicioNomina, mesNomina, numeroNomina, num_matEmpleado) ON DELETE CASCADE;
+
+ALTER TABLE ingreso ADD CONSTRAINT ck_ing_cod_nn CHECK(codigoConcepto_retributivo IS NOT NULL);
